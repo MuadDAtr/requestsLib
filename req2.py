3bb88f416f3e396761f8a0b33d92a90a722b3b5d
MuadDAtr <int.stron@protonmail.com>
2023-01-24 22:18:02 +0100

    status codes

--- req2.py
+++ req2.py
@@ -1,5 +1,9 @@
 import requests
 
-response = requests.get("https://httpbin.org/status/200")
+response = requests.get("https://httpbin.org/status/300")
 
-print(response.status_code)
+
+if response.status_code == requests.codes.not_found:
+    print("Page not found")
+else: 
+    print(response.status_code)
